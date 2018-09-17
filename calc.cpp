926b480782f1de5da953328b15dd9d56a2643f15
Xing Tao Shi <xshi@stuy.edu>
2018-09-16 22:46:01 -0400

    b

---
+++ calc.cpp
@@ -0,0 +1,72 @@
+/*
+Author: Xing Tao Shi
+Course: CSCI-135
+Instructor: Maryash
+Assignment: Lab1B
+
+This program acts as a calculator for non negative integers
+and only processes +/-
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+  char c = 0;
+  int total = 0;
+  int ic = 0; // int version of char
+  int next_num = 0;
+  int operation = 0; // 0 is +, 1 is -
+  while(cin >> c) { // While the reading operation is a success                                                                                                                 
+    if (c == '+') { //afterwards
+      if (operation == 0) {
+	total += next_num;
+      }
+      else {
+	total -= next_num;
+      }
+      operation = 0;
+      next_num = 0;      
+    }
+    else if (c == '-') {
+      if (operation == 0) {
+	total += next_num;
+      }
+      else {
+	total -= next_num;
+      }
+      operation = 1;
+      next_num = 0;      
+    }
+    else {
+      next_num *= 10;
+      ic = c-48;
+      next_num += ic;      
+      //cout << "IC: " << ic << endl;
+      //cout << "C: " << c << endl;
+      //cout << next_num << endl;
+      //cout << total << endl;
+      //cout << "OP: " << operation << endl;
+    } 
+  }
+  
+  if (operation == 0) {
+    total += next_num;
+  }
+  else {
+    total -= next_num;
+  }
+
+  cout << total << endl; // print the total   
+  return 0;
+
+  /*
+  char c = 0;
+  while (cin >> c) {
+    cout << c << endl;
+  }
+  return 0;
+  */
+}
